cca1280ecea92f2b35f81e3ccd8b217c4e8e6621
julivicv <julivicv@gmail.com>
2022-05-08 19:18:31 -0300

    first commit

---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'secret'
+//shared
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john,peter}
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exports.items = ['item1', 'item2']
+const person = {
+    name: 'bob',
+}
+// same as the above
+module.exports.singlePerson = person
